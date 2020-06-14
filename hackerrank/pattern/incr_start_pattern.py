de45e43339cdcababdb0093c5f22aa802fe25f41
tanvir43 <raihncse43@gmail.com>
2020-06-14 13:05:40 +0600

    incremental start pattern

--- hackerrank/pattern/incr_start_pattern.py
+++ hackerrank/pattern/incr_start_pattern.py
@@ -6,16 +6,13 @@
 #****
 #*****
 
-def incr_star_patt(n):
+#SOULUTION
 
+def incr_start_patt(v):
     my_list = []
-    for i in range(1, n+1):
+    for i in range(1, v+1):
         my_list.append("*"*i)
     print("\n".join(my_list))
 
-
 if __name__ == '__main__':
-    incr_star_patt(6)
-
-
-
+    incr_start_patt(6)
