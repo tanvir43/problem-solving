9dd8cf36f1ad0a95c1c5e8a33ba36f47c2c6b46d
tanvir43 <raihncse43@gmail.com>
2020-06-26 07:07:58 +0600

    string formatting problem solving

---
+++ hackerrank/strings/alt_string_formatting.py
@@ -0,0 +1,33 @@
+"""
+Given an integer, , print the following values for each integer  from  to :
+
+Decimal
+Octal
+Hexadecimal (capitalized)
+Binary
+The four values must be printed on a single line in the order specified above for each  from  to . Each value should be space-padded to match the width of the binary value of .
+
+Sample Input
+
+17
+Sample Output
+
+    1     1     1     1
+    2     2     2    10
+    3     3     3    11
+    4     4     4   100
+    5     5     5   101
+    6     6     6   110
+    7     7     7   111
+    8    10     8  1000
+    9    11     9  1001
+   10    12     A  1010
+"""
+
+def string_formatting(number):
+    width = len("{0:b}".format(number))
+    for i in range(1, number+1):
+        print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width)
+
+if __name__ == "__main__":
+    string_formatting(10)
