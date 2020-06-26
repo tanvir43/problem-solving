d7b643721c11270754f1476702a0c40b607b07d9
tanvir43 <raihncse43@gmail.com>
2020-06-26 07:11:56 +0600

    string formatting problem solving bug fixed

--- hackerrank/strings/alt_string_formatting.py
+++ hackerrank/strings/alt_string_formatting.py
@@ -27,7 +27,7 @@ Sample Output
 def string_formatting(number):
     width = len("{0:b}".format(number))
     for i in range(1, number+1):
-        print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width)
+        print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
 
 if __name__ == "__main__":
     string_formatting(10)
